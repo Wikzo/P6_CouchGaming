0057f9ff589c2d1cd83d753a6902b9339dc9db9d
Wikzo <Linkthewindwaker@gmail.com>
2014-03-27 12:32:36 +0100

    IMission interface and KillMision class

---
+++ UnityGame/Assets/_!Scripts/MissionManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissionManager : MonoBehaviour
+{
+
+    public IMission Mission;
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
