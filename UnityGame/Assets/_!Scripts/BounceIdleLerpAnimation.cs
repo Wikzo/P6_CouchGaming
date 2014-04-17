c9e84e5bd45ec6df2e4fbd80574fd2e6abe67ac6
Wikzo <Linkthewindwaker@gmail.com>
2014-04-17 14:46:41 +0200

    Simple bounce/idle animation using lerp and local scale (for props such as USB key)

---
+++ UnityGame/Assets/_!Scripts/BounceIdleLerpAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceIdleLerpAnimation : MonoBehaviour
+{
+    private Vector3 start;
+    private float Offset = 0.2f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        start = gameObject.transform.localScale;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time / 2, Offset);
+        Vector3 add = new Vector3(lerp, lerp, lerp);
+        transform.localScale = start + add;
+    }
+
+
+}
