bfd5c97ca9b1cd9000636a32f1f929739fc4afb2
Benjamin Nicholas Overgaard <tarious@live.dk>
2014-05-01 13:18:52 +0200

    Varying Intensity should work

---
+++ UnityGame/Assets/_!Scripts/ControllerTester/ControllerTesterRumbleVaryingIntensity.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure;
+
+public class ControllerTesterRumbleVaryingIntensity : ControllerTesterRumble {
+
+	private float intensityTimer;
+
+	private float lowSpeed = 3;
+	private float highSpeed = 2;
+
+	private bool intensityTimerInitialized = false;
+	
+	public ControllerTesterRumbleVaryingIntensity(List<ControllerPlayer> controllerPlayers, ControllerTesterManager controllerManager) : base(controllerPlayers, controllerManager)
+	{
+
+	}
+
+	public override void StartRumble(int patternToUse)
+	{
+		base.StartRumble(patternToUse);
+	}
+    public override void UpdateRumble()
+    {
+		switch((int)pattern)
+		{
+			case 0:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 0;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer <= 1)
+				{
+					intensityTimer += manager.RumbleTimer/lowSpeed;
+					GamePad.SetVibration(PlayerIndex.One, intensityTimer, intensityTimer);
+				}
+				else
+					StopRumble();
+				break;
+			case 1:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 0;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer <= 1)
+				{
+					intensityTimer += manager.RumbleTimer/highSpeed;
+					GamePad.SetVibration(PlayerIndex.One, intensityTimer, intensityTimer);
+				}
+				else
+					StopRumble();
+				break;
+			case 2:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 1;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer >= 0.01f)
+				{
+					intensityTimer -= manager.RumbleTimer/lowSpeed;
+					GamePad.SetVibration(PlayerIndex.One, intensityTimer, intensityTimer);
+				}
+				else
+					StopRumble();
+				break;
+			case 3:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 1;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer >= 0.01f)
+				{
+					intensityTimer -= manager.RumbleTimer/highSpeed;
+					GamePad.SetVibration(PlayerIndex.One, intensityTimer, intensityTimer);
+				}
+				else
+					StopRumble();
+				break;
+		}
+    }
+}
---
+++ UnityGame/Assets/_!Scripts/ControllerTester/TempRumble.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class TempRumble : MonoBehaviour {
+
+	private int pattern = 1;
+	private float intensityTimer;
+
+	private float lowSpeed = 3;
+	private float highSpeed = 2;
+
+	private bool intensityTimerInitialized = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch(pattern)
+		{
+			case 0:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 0;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer <= 1)
+					intensityTimer += Time.deltaTime/lowSpeed;
+				break;
+			case 1:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 0;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer <= 1)
+					intensityTimer += Time.deltaTime/highSpeed;
+				break;
+			case 2:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 1;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer >= 0.01f)
+					intensityTimer -= Time.deltaTime/lowSpeed;
+				break;
+			case 3:
+				if(intensityTimerInitialized == false)
+				{
+					intensityTimer = 1;
+					intensityTimerInitialized = true;
+				}
+
+				if(intensityTimer >= 0.01f)
+					intensityTimer -= Time.deltaTime/highSpeed;
+				break;
+		}
+		GamePad.SetVibration(PlayerIndex.One, intensityTimer, intensityTimer);
+	}
+
+	void OnApplicationQuit()
+	{
+		GamePad.SetVibration(PlayerIndex.One, 0, 0);
+	}
+}
