523843fbad2520ec432a73b38112e5adbc1902d5
Wikzo <Linkthewindwaker@gmail.com>
2014-05-01 11:15:56 +0200

    Controller tester

---
+++ UnityGame/Assets/_!Scripts/ControllerTester/ControllerTesterRumble.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure;
+
+
+public abstract class ControllerTesterRumble
+{
+    List<PlayerIndex> playerIndex;
+
+    public ControllerTesterRumble(List<PlayerIndex> playerIndexes)
+    {
+        playerIndex = playerIndexes;
+    }
+    public abstract void StartRumble(int pattern);
+    public abstract void UpdateRumble();
+    
+    public void StopRumble()
+    {
+        foreach(PlayerIndex player in playerIndex)
+            GamePad.SetVibration(player, 0, 0);
+    }
+
+}
