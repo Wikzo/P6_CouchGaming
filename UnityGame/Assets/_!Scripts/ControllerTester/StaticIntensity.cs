47d5f8e723c6b6bc696957c35a2662cd3be2aca1
Wikzo <Linkthewindwaker@gmail.com>
2014-05-02 11:19:22 +0200

    Rumbles test done with logging and multiple input and paper checkbox

--- UnityGame/Assets/_!Scripts/ControllerTester/StaticIntensity.cs
+++ UnityGame/Assets/_!Scripts/ControllerTester/StaticIntensity.cs
@@ -39,7 +39,7 @@ public class StaticIntensity : ControllerTesterRumble
 
         }
 
-        if (manager.RumbleTimer > manager.RumbleInterval)
+        if (manager.RumbleTimer > RumbleDuration)
         {
             StopRumble();
         }
