a060617de4c0d0dbb1291a269d77c52ed2f4e08f
Benjamin Nicholas Overgaard <tarious@live.dk>
2014-05-12 14:19:05 +0200

    If a projectile is out of the camera's view (for longer time than it takes to screenwrap) or if a projectile is completely stuck inside an object so it cannot be seen, it is destroyed and the player gets another shot. NOTE: The Projectile Layer needs to be included in the PerspectiveView DirectionalLight CullingMask to get proper lighting.

---
+++ UnityGame/Assets/_!Scripts/StuckDetector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StuckDetector : MonoBehaviour {
+
+	public bool StuckInObject = false;
+	private StuckCollider[] stuckColliders;
+
+	// Use this for initialization
+	void Start () 
+	{
+		stuckColliders = GetComponentsInChildren<StuckCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(stuckColliders[0].IsColliding && stuckColliders[1].IsColliding && stuckColliders[2].IsColliding)
+			StuckInObject = true;
+	}
+}
