a913ab97db31f91ee44f3fef4488764c39736daa
Benjamin Nicholas Overgaard <tarious@live.dk>
2014-05-18 20:06:09 +0200

    Zone cone done, zone sound done, aim crosses done, a few other stuff

--- UnityGame/Assets/_!Scripts/Missions/ZoneTrigger.cs
+++ UnityGame/Assets/_!Scripts/Missions/ZoneTrigger.cs
@@ -18,6 +18,8 @@ public class ZoneTrigger : MonoBehaviour {
 	private int playersColliding = 0;
 	private float progressBarY = 0;
 
+	private bool hasPlayedSound = false;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -28,31 +30,60 @@ public class ZoneTrigger : MonoBehaviour {
 			progressBar = pTran.Find("HologramFront");
 
 		background = pTran.Find("HologramBack");
+
+		audio.clip = AudioManager.Instance.ZoneIncrease;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-        // only rumble if playing
-        if (GameManager.Instance.PlayingState != PlayingState.Playing)
-            return;
-
 		if(progressBar.localScale.z <= background.localScale.z && readyToTrigger == true)
 		{
 			if(playersColliding > 0)
+			{
 				progressCounter += Time.deltaTime;
+
+				if(hasPlayedSound == false)
+				{
+					audio.volume = 0.5f;
+					audio.Play();
+					hasPlayedSound = true;
+				}
+			}
 		}
 		else
 			Accomplished = true;
 
 		if(playersColliding == 0 && progressCounter > 0)
-				progressCounter -= Time.deltaTime/DownSlowFactor;
+		{
+			progressCounter -= Time.deltaTime/DownSlowFactor;
+
+			if(hasPlayedSound == false)
+			{
+				audio.volume = 0.5f;
+				audio.Play();
+				hasPlayedSound = true;
+			}
+		}
+		else if(playersColliding == 0 && progressCounter < 0)
+		{
+			audio.Stop();
+			hasPlayedSound = false;
+		}
 
 		if(readyToTrigger == false && playersColliding == 0)
 			readyToTrigger = true;
+		audio.pitch = ((progressCounter/AccomplishTime)*2)+1;
+		progressBar.localScale = new Vector3(progressBar.localScale.x, 0.75f + (progressCounter/AccomplishTime)/3, progressCounter/AccomplishTime);
 
-		progressBar.localScale = new Vector3(progressBar.localScale.x, progressBar.localScale.y, progressCounter/AccomplishTime);
 		//progressBar.position = new Vector3(progressBar.position.x, pTran.position.y-pTran.localScale.y/2+progressBar.localScale.y, progressBar.position.z);
+
+		// only rumble if playing
+        if (GameManager.Instance.PlayingState != PlayingState.Playing)
+        {
+            audio.Stop();
+            return;
+        }
 	}
 
 	void OnTriggerEnter(Collider collider)
@@ -69,11 +100,10 @@ public class ZoneTrigger : MonoBehaviour {
 
 	public void RestartZone()
 	{
+		audio.Stop();
+		hasPlayedSound = false;
 		Accomplished = false;
 		readyToTrigger = false;
 		progressCounter = 0;
-
-        // TODO: for BENJAMIN:
-        // this reset function does not work! I call it from MissionZone.cs, but it needs to reset the progressBar scale things
 	}
 }
